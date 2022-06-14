0f678873e9448cbb5c3770fe2e901e87592ae8ab
Ramon Meffert <ramon.meffert@gmail.com>
2022-06-14 14:34:36 +0200

    Clean up repo and add first version of readme

---
+++ README.md
@@ -0,0 +1,71 @@
+# A Comparison of Prompt Engineering Methods for Checking Verifiability of Online User Comments
+
+Md Zobaer Hossain, Linfei Zhang, Robert van Timmeren and Ramon Meffert, June 2022
+
+This repository contains the source code for the experiments, data processing
+and data analysis conducted as part of our course project for the 2021–2022
+edition of the _Language Technology Project_ course at the University of
+Groningen.
+
+## Data
+
+All files related to the datasets are located in the [datasets](datasets/)
+folder. We have taken the original dataset files and transformed them into the
+[HuggingFace dataset format][hf-datasets]. All dataset folders contain the
+original dataset files, an analysis notebook and a demo file showing how you use
+the dataset.
+
+### [Regulation Room](datasets/regulation_room/)
+
+The [original dataset files][rr-dataset] used by Park & Cardie (2014) are
+available from Joonsuk Park's [website][rr-site]. The code used to transform the
+provided `.txt` files into `.csv` files that can be used with HuggingFace
+datasets is available in [`preprocess.py`][rr-pp] and the dataset file is
+available in [`regulation_room.py`][rr-hf]. Exploratory analysis is available in
+[`analysis.ipynb`][rr-nb].
+
+### [Change My View](datasets/change_my_view/)
+
+This is an as yet unpublished dataset created at the University of Groningen.
+The original data files were already in `.csv` format, and the dataset file is
+available in [`change_my_view.py`][cmv-hf]. Exploratory analysis is available in
+[`analysis.ipynb`][cmv-nb].
+
+### [Merged dataset](datasets/verifiability/)
+
+To combat the small size of the datasets, we have created a merged dataset from
+the two datasets mentioned above. The code used to generate the `.csv` files for
+the splits is available in [`generate.py`][merged-pp], and the dataset file is
+available in [`verifiability.py`][merged-hf]. Exploratory analysis is available
+in [`analysis.ipynb`][merged-nb].
+
+## Experiments
+
+All code for experiments is located in the [experiments](experiments/) folder.
+
+- Baseline: BERT
+- (i)PET
+- LM-BFF
+- Prompt Training
+
+---
+
+## References
+
+Park, J., & Cardie, C. (2014). Identifying Appropriate Support for Propositions
+in Online User Comments. _Proceedings of the First Workshop on Argumentation
+Mining_, 29–38. <https://doi.org/10/gg29gq>
+
+
+<!-- URLs -->
+
+[hf-datasets]: https://huggingface.co/docs/datasets/dataset_script
+[rr-site]: https://facultystaff.richmond.edu/~jpark/
+[rr-dataset]: https://facultystaff.richmond.edu/~jpark/data/jpark_aclw14.zip
+[rr-hf]: datasets/regulation_room/regulation_room.py
+[rr-pp]: datasets/regulation_room/preprocess.py
+[cmv-hf]: datasets/change_my_view/change_my_view.py
+[cmv-nb]: datasets/change_my_view/analysis.ipynb
+[merged-hf]: datasets/verifiability/verifiability.py
+[merged-pp]: datasets/verifiability/generate.py
+[merged-nb]: datasets/verifiability/analysis.ipynb
---
+++ experiments/PromptTraining.py
@@ -0,0 +1,126 @@
+from datasets import load_dataset
+from openprompt.plms import load_plm
+from openprompt.prompts import ManualVerbalizer
+from openprompt import PromptForClassification
+from openprompt.data_utils import InputExample
+from openprompt import PromptDataLoader
+import torch
+from transformers import AdamW
+from sklearn.metrics import classification_report
+from openprompt.prompts import ManualTemplate
+
+plm, tokenizer, model_config, WrapperClass = load_plm('roberta',
+                                                      "roberta-base")
+
+promptTemplate = ManualTemplate(
+    text='{"placeholder":"text_a"} This text {"mask"} key information.',
+    tokenizer=tokenizer,
+)
+classes = ['u', 'v']
+
+promptVerbalizer = ManualVerbalizer(
+    classes=classes,
+    label_words={
+        "u": ["lacks", "needs", "wants"],
+        "v": ["contains", "has", "provides"],
+    },
+    tokenizer=tokenizer,
+)
+
+promptModel = PromptForClassification(
+    template=promptTemplate,
+    plm=plm,
+    verbalizer=promptVerbalizer,
+)
+
+train = load_dataset("../datasets/regulation_room", split="train[:90%]")
+dev = load_dataset("../datasets/regulation_room", split="train[-10%:]")
+test = load_dataset("../datasets/regulation_room", split="test")
+raw_dataset = {'train': train, 'validation': dev, 'test': test}
+
+dataset = {}
+for split in ['train', 'test', 'validation']:
+    dataset[split] = []
+    for i, data in enumerate(raw_dataset[split]):
+        if data['label'] == 'u':
+            label = 0
+        else:
+            label = 1
+        input_example = InputExample(text_a=data['text'], label=label, guid=i)
+        dataset[split].append(input_example)
+
+train_data_loader = PromptDataLoader(
+    dataset=dataset['train'],
+    tokenizer=tokenizer,
+    template=promptTemplate,
+    tokenizer_wrapper_class=WrapperClass,
+    batch_size=4
+    # max_seq_length=256, decoder_max_length=3,
+    # batch_size=4,shuffle=True, teacher_forcing=False, 
+    # predict_eos_token=False, truncate_method="head"
+)
+
+loss_func = torch.nn.CrossEntropyLoss()
+no_decay = ['bias', 'LayerNorm.weight']
+# it's always good practice to set no decay to biase and LayerNorm parameters
+optimizer_grouped_parameters = [{
+    'params': [
+        p for n, p in promptModel.named_parameters()
+        if not any(nd in n for nd in no_decay)
+    ],
+    'weight_decay':
+    0.01
+}, {
+    'params': [
+        p for n, p in promptModel.named_parameters()
+        if any(nd in n for nd in no_decay)
+    ],
+    'weight_decay':
+    0.0
+}]
+
+optimizer = AdamW(optimizer_grouped_parameters, lr=1e-4)
+use_cuda = True
+if use_cuda:
+    promptModel = promptModel.cuda()
+for epoch in range(5):
+    tot_loss = 0
+    for step, inputs in enumerate(train_data_loader):
+        if use_cuda:
+            inputs = inputs.cuda()
+        logits = promptModel(inputs)
+        labels = inputs['label']
+        loss = loss_func(logits, labels)
+        loss.backward()
+        tot_loss += loss.item()
+        optimizer.step()
+        optimizer.zero_grad()
+        if step % 1000 == 1:
+            print("Epoch {}, average loss: {}".format(epoch,
+                                                      tot_loss / (step + 1)),
+                  flush=True)
+
+test_data_loader = PromptDataLoader(
+    dataset=dataset['test'],
+    tokenizer=tokenizer,
+    template=promptTemplate,
+    tokenizer_wrapper_class=WrapperClass,
+    # max_seq_length=256, decoder_max_length=3,
+    # batch_size=1,shuffle=False, teacher_forcing=False,
+    # predict_eos_token=False, truncate_method="head"
+)
+
+use_cuda = True
+predicted = []
+promptModel.eval()
+with torch.no_grad():
+    for batch in test_data_loader:
+        if use_cuda:
+            batch = batch.cuda()
+        logits = promptModel(batch)
+        preds = torch.argmax(logits, dim=-1)
+        #         print(preds)
+        predicted.append(classes[preds])
+
+true_labels = raw_dataset['test']['label']
+print(classification_report(true_labels, predicted))
